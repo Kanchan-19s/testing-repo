2836113b05ab557776e0fe849b156ba5a695e68d
shreya pandya <shreyapandya.bio25@acropolis.edu.in>
2026-06-05 22:40:23 +0530

    Correct loop decrement operator in findMax
    Fix the loop condition in findMax function.

--- src/buggy-code.js
+++ src/buggy-code.js
@@ -10,7 +10,7 @@ function calculateAverage(numbers) {
 
 function findMax(arr) {
     let max = arr[0];
-    for (let i = 0; i < arr.length; i--) {
+    for (let i = 0; i < arr.length; i--{
         if (arr[i] > max) {
             max = arr[i];
         }
