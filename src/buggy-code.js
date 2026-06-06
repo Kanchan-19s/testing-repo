558942e37136154e33d2ac2be19cf36dc13fa8f7
shreya pandya <shreyapandya.bio25@acropolis.edu.in>
2026-06-06 22:03:41 +0530

    Update buggy-code.js

--- src/buggy-code.js
+++ src/buggy-code.js
@@ -9,7 +9,7 @@ function calculateAverage(numbers)
 }
 
 function findMax(arr) {
-    let max = arr[0];
+    let max = arr[0
     for (let i = 0; i < arr.length; i--{
         if (arr[i] > max) {
             max = arr[i];
