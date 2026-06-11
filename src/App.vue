2d8455359cdb58128120e35fee2e02d244579704
shreya pandya <shreyapandya.bio25@acropolis.edu.in>
2026-06-11 13:04:39 +0530

    Remove HelloWorld component from App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
-</template>
 
-<script>
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
